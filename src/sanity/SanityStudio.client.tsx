7c2f42c3b2476a4ff14d3719f4be4856997b4305
Noah Gentile <noah.gentile@gmail.com>
2022-11-22 14:32:25 -0800

    fix: incorrect import path

--- src/sanity/SanityStudio.client.tsx
+++ src/sanity/SanityStudio.client.tsx
@@ -1,6 +1,6 @@
 import {Studio} from 'sanity';
 import config from '../../sanity.config';
-import './studio.css';
+import './SanityStudio.css';
 
 export default function SanityStudio() {
   return <Studio config={config} />;
