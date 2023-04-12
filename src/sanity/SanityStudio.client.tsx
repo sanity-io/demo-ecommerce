e898c9edd136157199ac9f7fbec14911cc25a1f7
Noah Gentile <noah.gentile@gmail.com>
2023-04-11 20:11:56 -0700

    feat: disable auth

--- src/sanity/SanityStudio.client.tsx
+++ src/sanity/SanityStudio.client.tsx
@@ -3,5 +3,5 @@ import config from '../../sanity.config';
 import './SanityStudio.css';
 
 export default function SanityStudio() {
-  return <Studio config={config} />;
+  return <Studio config={config} unstable_noAuthBoundary={true} />;
 }
