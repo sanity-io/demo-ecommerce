b758d67f0a2284ae6a2c0a7e4853788727655936
Noah Gentile <noah.gentile@gmail.com>
2023-06-09 18:39:40 -0700

    fix: adjust environment variables

--- apps/storefront/handler.ts
+++ apps/storefront/handler.ts
@@ -59,7 +59,7 @@ export async function handler(
       publicStorefrontToken: env.PUBLIC_STOREFRONT_API_TOKEN,
       privateStorefrontToken: env.PRIVATE_STOREFRONT_API_TOKEN,
       storeDomain: `https://${env.PUBLIC_STORE_DOMAIN}`,
-      storefrontApiVersion: env.PUBLIC_STOREFRONT_API_VERSION || "2023-01",
+      storefrontApiVersion: env.PUBLIC_STOREFRONT_API_VERSION || "2023-04",
       storefrontId: env.PUBLIC_STOREFRONT_ID,
       storefrontHeaders: getStorefrontHeaders(request),
     });
@@ -78,8 +78,8 @@ export async function handler(
       // Pass configuration options for Sanity client
       config: {
         projectId: env.SANITY_PROJECT_ID,
-        dataset: env.SANITY_DATASET,
-        apiVersion: env.SANITY_API_VERSION ?? "2023-03-30",
+        dataset: env.SANITY_DATASET || "production",
+        apiVersion: env.SANITY_API_VERSION || "2023-03-30",
         useCdn: process.env.NODE_ENV === "production",
       },
     });
