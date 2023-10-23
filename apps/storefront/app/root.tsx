76fae6513c7dedfdd73be5dd24682c3aa49a94ee
Tom Smith <tom@sanity.io>
2023-10-23 16:51:44 +0100

    fix: addition meta not needed

--- apps/storefront/app/root.tsx
+++ apps/storefront/app/root.tsx
@@ -187,7 +187,6 @@ export function ErrorBoundary({ error }: { error: Error }) {
     <html lang={locale.language}>
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
         <title>{title}</title>
         <Meta />
         <Links />
