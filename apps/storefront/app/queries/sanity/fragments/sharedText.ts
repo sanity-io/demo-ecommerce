b663211c1eaddb8e54880f85a96da229edee2833
Tom Smith <tom@sanity.io>
2023-10-25 10:33:01 +0100

    fix: missing sharedText

--- apps/storefront/app/queries/sanity/fragments/sharedText.ts
+++ apps/storefront/app/queries/sanity/fragments/sharedText.ts
@@ -1,7 +1,7 @@
 import groq from "groq";
 
 export const SHARED_TEXT = groq`
-  "sharedText": *[_type == 'settings'][0] {
+  "sharedText": *[_type == 'sharedText'][0] {
     "deliveryAndReturns": coalesce(deliveryAndReturns[_key == $language][0].value, deliveryAndReturns[_key == $baseLanguage][0].value)[]
   }
 `;
