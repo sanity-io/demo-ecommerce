c756583ff1e42d0f7819ce4e0aaa2aa756781676
Cody Olsen <stipsan@gmail.com>
2023-11-08 23:49:59 +0100

    fix: stable conservative stega filtering

--- apps/storefront/app/lib/sanity/useQueryStore.ts
+++ apps/storefront/app/lib/sanity/useQueryStore.ts
@@ -1,15 +1,20 @@
 import type { StegaConfig } from "@sanity/client/stega";
 import { createQueryStore } from "@sanity/react-loader";
 
-const denylist = new Set(["seo", "variantGid"]);
+const denyList = new Set(["seo"]);
+const allowList = new Set(["title", "text", "body"]);
 
 export const stegaFilter: StegaConfig["filter"] = (props) => {
   if (
     props.sourcePath.some((path) =>
-      typeof path === "string" ? denylist.has(path) : false
+      typeof path === "string" ? denyList.has(path) : false
     )
   ) {
     return false;
+  }
+  const endPath = props.sourcePath.at(-1);
+  if (typeof endPath === "string" && allowList.has(endPath)) {
+    return true;
   }
   return props.filterDefault(props);
 };
