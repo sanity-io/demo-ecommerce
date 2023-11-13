e2e8839cbb8f655b132e382ee4a65271d298c517
Knut Melvær <knut@sanity.io>
2023-11-13 13:51:53 -0800

    chore: use composer dataset in cli

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -3,6 +3,6 @@ import { defineCliConfig } from "sanity/cli";
 export default defineCliConfig({
   api: {
     projectId: "k4hg38xw",
-    dataset: "production",
+    dataset: "composer-2",
   },
 });
