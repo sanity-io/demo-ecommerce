0878db317ad8abb8241400052d36a10ebcad2324
Noah Gentile <noah.gentile@gmail.com>
2023-11-22 07:57:53 -0800

    fix: adjust csp

--- apps/storefront/app/entry.server.tsx
+++ apps/storefront/app/entry.server.tsx
@@ -1,6 +1,6 @@
 import { RemixServer } from "@remix-run/react";
 import { createContentSecurityPolicy } from "@shopify/hydrogen";
-import type { EntryContext } from "@shopify/remix-oxygen";
+import type { AppLoadContext, EntryContext } from "@shopify/remix-oxygen";
 import isbot from "isbot";
 import { renderToReadableStream } from "react-dom/server";
 
@@ -8,14 +8,31 @@ export default async function handleRequest(
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
-  remixContext: EntryContext
+  remixContext: EntryContext,
+  context: AppLoadContext
 ) {
+  const { SANITY_PROJECT_ID: projectId } = context.env;
+  /**
+   * @see https://shopify.dev/docs/api/hydrogen/2023-10/utilities/createcontentsecuritypolicy
+   */
   const { nonce, header, NonceProvider } = createContentSecurityPolicy({
-    imgSrc: ["https://cdn.shopify.com", "https://cdn.sanity.io"],
+    imgSrc: [
+      `'self'`,
+      "https://cdn.shopify.com",
+      "https://cdn.sanity.io",
+      "https://lh3.googleusercontent.com",
+    ],
     styleSrc: [`'self'`, `'unsafe-inline'`, "https://fonts.googleapis.com"],
     scriptSrc: [`'self'`, "www.instagram.com"],
-    fontSrc: ["https://fonts.gstatic.com"],
-    frameSrc: ["https://www.instagram.com"],
+    fontSrc: [`'self'`, "https://fonts.gstatic.com"],
+    frameAncestors: [`'self'`],
+    frameSrc: [`'self'`, "https://www.instagram.com"],
+    connectSrc: [
+      `'self'`,
+      "https://monorail-edge.shopifysvc.com",
+      `https://${projectId}.api.sanity.io`,
+      `wss://${projectId}.api.sanity.io`,
+    ],
   });
 
   const body = await renderToReadableStream(
